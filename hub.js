2d7012fe432f81042c76b81c1f8f0f69d47d2a4a
syukurhamdani <duhrimuhammad111@gmail.com>
2025-04-15 07:00:15 +0700

    Add files via upload

---
+++ hub.js
@@ -0,0 +1,20 @@
+let nomorplat = [`B 3933 VNB`, `AA 3040 SI`, `A 2938 KWQ`, `G 2463 KAD`];
+
+function cekganjilgenap(nomor) {
+    let angka = parseInt(nomor.split(" ")[1]); // Hanya 1 spasi
+    let hasil;
+
+    switch (angka % 2) {
+        case 0:
+            hasil = `genap`;
+            break;
+        case 1:
+            hasil = `ganjil`;
+            break;
+        default:
+            hasil = `tidak valid`;
+    }
+    console.log(`${nomor} itu plat ${hasil}`);
+}
+
+nomorplat.forEach(nomor => cekganjilgenap(nomor));
